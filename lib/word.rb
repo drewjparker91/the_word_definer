fa3a5cf9ba9456291bd83e8f6c7520f6be83e107
drewparker <drewjparker91@gmail.com>
2020-10-02 09:05:14 -0700

    create test and receive good fail for Word.all

---
+++ lib/word.rb
@@ -0,0 +1,22 @@
+require 'pry'
+
+class Word
+  attr_reader :title, :id
+
+  @@words = {}
+  @@total_rows = 0
+
+  def initialize (attributes)
+    @title = attributes.fetch(:title)
+    @id = attributes.fetch(:id) || @@total_rows += 1
+  end
+
+  def self.all
+    @@words.values()
+  end
+
+  def self.clear
+    @@words = {}
+    @@total_rows = 0
+  end
+end
---
+++ spec/word_spec.rb
@@ -0,0 +1,14 @@
+require 'rspec'
+require 'word'
+
+describe '#Word' do
+  before(:each) do
+    Word.clear()
+  end
+
+  describe ('.all') do
+    it ('returns an empty array when there are no words') do
+      expect(Word.all).to(eq(1))
+    end
+  end
+end
