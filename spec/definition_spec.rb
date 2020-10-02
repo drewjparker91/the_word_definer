40f82ca0fa286075969bb566f9a10e08f59b42a4
drewparker <drewjparker91@gmail.com>
2020-10-02 10:43:50 -0700

    create test for Definition.clear, get good fail and good pass

--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -14,11 +14,19 @@ describe '#Definition' do
 
   describe ('#save') do
     it("saves a definition") do
-      word = Definition.new({:body => "Hello", :id => nil})
-      word.save()
-      expect(Definition.all).to(eq([word]))
+      definition = Definition.new({:body => "Hello", :id => nil})
+      definition.save()
+      expect(Definition.all).to(eq([definition]))
     end
   end
 
+  describe ('#clear') do
+    it("clears a definition") do
+      definition = Definition.new({:body => "Hello", :id => nil})
+      definition.save()
+      Definition.clear()
+      expect(Definition.all).to(eq([]))
+    end
+  end
 end
 
