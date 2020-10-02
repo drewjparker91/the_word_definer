80bf0b56cad91b0a42defdb456e2b8af618365ad
drewparker <drewjparker91@gmail.com>
2020-10-02 10:34:23 -0700

    create word.erb

--- app.rb
+++ app.rb
@@ -34,3 +34,15 @@ post('/words') do
   erb(:words)
 end
 
+# POSTING DEFINITION TO SPECIFIC WORD
+# post('/boards/:id') do
+#   @board = Board.find(params[:id].to_i)
+#   @title = params[:message_name]
+#   @body = params[:message_body]
+#   @message = Message.new({:title => @title, :body => "#{@body}", :board_id => ("#{@board.id}").to_i, :time => nil, :id => nil})
+#   @message.save
+#   @messages = Message.find_by_board(params[:id].to_i)
+#   erb(:board)
+# end
+
+
