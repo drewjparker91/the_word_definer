6addb833714be5dca8b2085027a90e9fc98be21b
drewparker <drewjparker91@gmail.com>
2020-10-02 10:03:21 -0700

    create get('/words'), get('/words/:id'), delete('/word/:id), and post('/words')

--- app.rb
+++ app.rb
@@ -9,3 +9,28 @@ get('/') do
   erb(:words)
 end
 
+get('/words') do
+  @words = Word.all
+  erb(:words)
+end
+
+get('/words/:id') do
+  @word = Word.find(params[:id].to_i())
+  erb(:word)
+end
+
+delete('/word/:id') do
+  @word = Word.find(params[:id].to_i())
+  @word.delete()
+  @words = Word.all
+  erb(:words)
+end
+
+post('/words') do
+  @title = params[:word_title]
+  @word = Word.new({:title => "#{@title}", :id => nil})
+  @word.save
+  @words = Word.all
+  erb(:words)
+end
+
