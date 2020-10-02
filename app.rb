f97c067dc99cc2f485ce21aa99b8f87b3711197d
drewparker <drewjparker91@gmail.com>
2020-10-02 16:23:16 -0700

    broke the code

--- app.rb
+++ app.rb
@@ -39,20 +39,60 @@ end
 post('/words/:id') do
   @word = Word.find(params[:id].to_i)
   @body = params[:definition_body]
-  @definition = Definition.new({:body => "#{body}", :id => nil, :word_id => ("#{@word.id}").to_i })
+  puts '>>> word: %s' % [@word.title]
+  puts '>>> body: %s' % [@body]
+  @definition = Definition.new({:body => "#{@body}", :id => nil, :word_id => ("#{@word.id}").to_i })
   @definition.save
   @definitions = Definition.find_by_word(params[:id].to_i)
   erb(:word)
 end
-# POSTING DEFINITION TO SPECIFIC WORD
-# post('/boards/:id') do
-#   @board = Board.find(params[:id].to_i)
-#   @title = params[:message_name]
-#   @body = params[:message_body]
-#   @message = Message.new({:title => @title, :body => "#{@body}", :board_id => ("#{@board.id}").to_i, :time => nil, :id => nil})
-#   @message.save
-#   @messages = Message.find_by_board(params[:id].to_i)
-#   erb(:board)
+
+get('/words/:id/edit') do
+  @word = Word.find(params[:id].to_i)
+  erb(:edit_word)
+end
+
+get('word/:id/definition/:definition_id') do
+  @definition = Definition.find(params[:definition_id].to_i)
+  @word_id = params[:id].to_i
+  erb(:edit_definition)
+end
+
+patch('/words/:id') do
+  @word = Word.find(params[:id].to_i())
+  title = params[:title]
+  @word.update_title(title)
+  @definitions = Definition.find_by_word(params[:id].to_i)
+  # @words = Word.all
+  erb(:word)
+end
+
+patch('/definitions/:id/edit') do
+  @definition = Definition.find(params[:id].to_i())
+  body = params[:body]
+  @definition.update_body(body)
+  @definitions = Definition.find_by_word(params[:id].to_i)
+  erb(:edit_definition)
+end
+
+get('/definitions/:definition_id') do 
+  @definition = Definition.find(params[:definition_id].to_i)
+  @word_id = params[:id]
+  erb(:definition)
+end
+
+delete('/definitions/:id') do
+  @definition = Definition.find(params[:id].to_i)
+  @definition.delete()
+  @word = Word.find(params[:id].to_i)
+  erb(:word)
+end
+
+# delete('/word/:id') do
+#   @word = Word.find(params[:id].to_i())
+#   @word.delete()
+#   @words = Word.all
+#   erb(:words)
 # end
 
 
